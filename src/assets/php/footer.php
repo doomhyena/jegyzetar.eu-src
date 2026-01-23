8cde998fe09f740c530f17a53543165bfe6d0b7c
Csontos Kincső <csontoskincso05@proton.me>
2026-01-23 14:38:02 +0100

    Valamiért lemaradtam a linkelésből?

--- src/assets/php/footer.php
+++ src/assets/php/footer.php
@@ -6,7 +6,7 @@
     <div class="footer-inner">
         <p>
             <?= t('footer_developers_label') ?>:
-            <a href="https://github.com/baranyi0" target="_blank" rel="noopener noreferrer">Baranyi Norbert</a>, <a href="https://github.com/PaladiTech" target="_blank" rel="noopener noreferrer">Szekeres Levente</a>
+            <a href="https://github.com/baranyi0" target="_blank" rel="noopener noreferrer">Baranyi Norbert</a>, <a href="https://doomhyena.hu/" target="_blank" rel="noopener noreferrer">Csontos Kincső Anasztázia</a>, <a href="https://github.com/PaladiTech" target="_blank" rel="noopener noreferrer">Szekeres Levente</a>
         </p>
         <a href="https://github.com/doomhyena/jegyzetar.eu"
            target="_blank"
